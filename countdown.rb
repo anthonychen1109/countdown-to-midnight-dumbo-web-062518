e3ea6da9dfb087b024f4259584b3d284b2db5c32
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-25 21:31:52 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -2,3 +2,16 @@
 
 def countdown
 end
+#write your code here
+
+def countdown(number)
+  
+  while number > 0
+    if number == 0
+      "HAPPY NEW YEAR!"
+    else
+      "#{number} SECOND(S)!"
+      number -= 1
+    end
+  end
+end
