5c5739f658184479bc72630428df437385aff24c
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-25 21:32:51 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,17 +1,10 @@
 #write your code here
 
-def countdown
-end
-#write your code here
-
 def countdown(number)
-  
+
   while number > 0
-    if number == 0
-      "HAPPY NEW YEAR!"
-    else
-      "#{number} SECOND(S)!"
-      number -= 1
-    end
+    "#{number} SECOND(S)!"
+    number -= 1
   end
+  "HAPPY NEW YEAR!"
 end
